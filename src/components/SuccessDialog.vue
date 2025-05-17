dec436518291755a922f1e1a528cec5653d20717
Adilson Oliveira <adilsonojr@gmail.com>
2025-05-17 05:59:45 -0300

    fix: update SuccessDialog component to use dynamic visibility

--- src/components/SuccessDialog.vue
+++ src/components/SuccessDialog.vue
@@ -1,6 +1,6 @@
 <template>
     <Transition name="dialog">
-        <div v-if="true" class="dialog-container">
+        <div v-if="show" class="dialog-container">
             <div class="dialog-content">
                 <svg class="check-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7" />
